9ab35142c06cb987b795838e9fa57338be7ad0ee
1juyoung <myjuyoung2@gmail.com>
2025-07-11 22:42:40 +0900

    feat: 카카오 소셜 로그인 연동

--- app/(guest)/login/page.tsx
+++ app/(guest)/login/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
 
 export default function LoginPage() {
+  const handleKakaoLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`;
+  };
+
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
       {/* 로고 부분 */}
@@ -15,12 +21,14 @@ export default function LoginPage() {
       </div>
       {/* 소셜 로그인 버튼 부분 */}
       <div className="flex flex-col gap-5">
-        <Image
-          src="/images/kakao-login.png"
-          width={300}
-          height={50}
-          alt="카카오 로그인"
-        />
+        <div onClick={handleKakaoLogin}>
+          <Image
+            src="/images/kakao-login.png"
+            width={300}
+            height={50}
+            alt="카카오 로그인"
+          />
+        </div>
         <Image
           src="/images/github-login.png"
           width={300}
