50d5621d2fa941ca07792fd821f3010f240857e2
1juyoung <myjuyoung2@gmail.com>
2025-07-14 10:27:55 +0900

    feat:팔로우 팔로잉 수 api 연동

--- app/(user)/my/components/Profile.tsx
+++ app/(user)/my/components/Profile.tsx
@@ -2,29 +2,31 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Github, Mail } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/MyTooltip";
+import { useFollowCount } from "@/app/api/my/useFollower";
 
-const user = {
-  name: "서현우",
-  email: "seohyun@example.com",
-  github: "https://github.com/seohyun",
-  introduce: "안녕하세요, 서현우입니다. 개발자입니다.",
-  image: "/images/sample-image.jpg",
-  followers: 120,
-  following: 80,
-  purchaseProjects: 5,
-  donation: 10,
-};
+interface User {
+  name: string;
+  email?: string;
+  portfolioAddress?: string;
+  image?: string;
+  introduce?: string;
+  followers?: number;
+  following?: number;
+}
 
 interface ProfileProps {
+  user: User;
   onEditClick: (isEditing: boolean) => void;
 }
-export function Profile({ onEditClick }: ProfileProps) {
+export function Profile({ user, onEditClick }: ProfileProps) {
+  const followData = useFollowCount();
+
   return (
     <div className="flex flex-row items-center justify-between pt-[20px] pd-[20px]">
       <div className="flex flex-row items-center gap-8">
         <div className="items-center justify-center">
           <Image
-            src={user.image}
+            src={user.image || "/images/sample-image.jpg"}
             width={100}
             height={100}
             alt="Profile Picture"
@@ -35,10 +37,10 @@ export function Profile({ onEditClick }: ProfileProps) {
         <div className="grid grid-rows-2 gap-y-0">
           <div className="flex flex-row items-center gap-1">
             <h3 className="text-lg font-bold mt-1">{user.name}</h3>
-            {user.github ? (
+            {user.portfolioAddress ? (
               <div className="w-18 h-5 bg-secondary rounded-full flex items-center justify-center text-white">
                 <Github className="h-4 w-4" />
-                <span className="text-xs ml-1">GitHub</span>
+                <span className="text-xs ml-1">portfolio</span>
               </div>
             ) : null}
           </div>
@@ -53,25 +55,30 @@ export function Profile({ onEditClick }: ProfileProps) {
             </Tooltip>
             <p className="text-sm">{user.email}</p>
           </div>
+
           <div className="grid grid-cols-4 gap-10 text-center">
             <div className="grid grid-rows-2">
               <p className="text-[#868e96] text-sm">팔로잉</p>
-              <p className="font-bold">{user.following}</p>
+              <p className="font-bold">
+                {followData?.data?.followerCount ?? "0"}
+              </p>
             </div>
 
             <div className="grid grid-rows-2">
-              <p className="text-[#868e96] text-sm">팔로워</p>{" "}
-              <p className="font-bold">{user.followers}</p>
+              <p className="text-[#868e96] text-sm">팔로워</p>
+              <p className="font-bold">
+                {followData?.data?.followingCount ?? "0"}
+              </p>
             </div>
 
             <div className="grid grid-rows-2">
-              <p className="text-[#868e96] text-sm">구매수</p>{" "}
-              <p className="font-bold">{user.purchaseProjects}</p>
+              <p className="text-[#868e96] text-sm">구매수</p>
+              <p className="font-bold">0</p>
             </div>
 
             <div className="grid grid-rows-2">
-              <p className="text-[#868e96] text-sm">후원수</p>{" "}
-              <p className="font-bold">{user.donation}</p>
+              <p className="text-[#868e96] text-sm">후원수</p>
+              <p className="font-bold">0</p>
             </div>
           </div>
         </div>
