353ef1c6fee2df4bf302a88a112042bb1e0e4f58
Hyunwoo-Seo <shw1801@naver.com>
2025-07-14 22:58:47 +0900

    fix: 배포를 위한 프리티어 무시설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["yetdas3.s3.ap-northeast-2.amazonaws.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
