116086cc9bee5fc4a47580f2110e5fa612e1c044
1juyoung <myjuyoung2@gmail.com>
2025-07-15 19:32:28 +0900

    feat: 로그인 branch 페이지 수정

--- app/(guest)/login/page.tsx
+++ app/(guest)/login/page.tsx
@@ -1,10 +1,49 @@
-import { cookies } from "next/headers";
-import LoginClient from "./components/LoginClient";
+"use client";
 
-export default async function Page() {
-  const cookieStore = await cookies();
-  console.log(cookieStore, "쿠키");
-  const token = cookieStore.get("accessToken")?.value ?? "";
-  console.log("쿠키 토큰 값", token);
-  return <LoginClient initialToken={token} />;
+import Image from "next/image";
+
+export default function LoginPage() {
+  const redirectUri = encodeURIComponent("http://localhost:3000"); // 로그인 후 리다이렉트할 URL
+
+  const handleKakaoLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao?state=${redirectUri}`;
+  };
+
+  const handleGithubLogin = () => {
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/github`;
+  };
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
+      {/* 로고 부분 */}
+      <div className="flex flex-col h-fit items-center justify-center gap-3">
+        <div className="text-md-navItem">필요한 작품에 엤다</div>
+        <Image
+          src="/images/sample-logo.png"
+          width={125}
+          height={64}
+          alt="logo"
+        />
+      </div>
+      {/* 소셜 로그인 버튼 부분 */}
+      <div className="flex flex-col gap-5">
+        <div onClick={handleKakaoLogin}>
+          <Image
+            src="/images/kakao-login.png"
+            width={300}
+            height={50}
+            alt="카카오 로그인"
+          />
+        </div>
+
+        <div onClick={handleGithubLogin}>
+          <Image
+            src="/images/github-login.png"
+            width={300}
+            height={50}
+            alt="깃허브 로그인"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
