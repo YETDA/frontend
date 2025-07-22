bf4179e2032a3d50dc2ae2ecd5881e4328b4e6ce
1juyoung <myjuyoung2@gmail.com>
2025-07-22 23:16:56 +0900

    feat: root layout에 SSE Provider 추가 - 열려 있는 모든 페이지에 알림이 보이기 위함

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import SubHeader from "@/components/SubHeader";
+import { SSEProvider } from "../providers/SSEProvider";
 
 export default function Layout({
   children,
@@ -12,7 +13,7 @@ export default function Layout({
       <Header />
       <SubHeader />
       <main className="w-full md:px-10 lg:px-20 xl:px-32 xxl:px-40 xxxl:px-[320px] max-w-screen-2xl mx-auto">
-        {children}
+        <SSEProvider>{children}</SSEProvider>
       </main>
       <Footer />
     </>
