ad64bbf36689501104bc50eafa12457c389c920f
1juyoung <myjuyoung2@gmail.com>
2025-07-22 23:15:21 +0900

    feat: addEventListener, onmessage 로직 추가

--- apis/alram/useConnectAlarm.ts
+++ apis/alram/useConnectAlarm.ts
@@ -22,6 +22,17 @@ export function useConnectAlarm() {
       setStatusData("ERROR");
     };
 
+    // 연결 확인용 이벤트로 인해 addEventListener 사용
+    evtSource.addEventListener("connect", e => {
+      console.log("connect 이벤트 수신:", e);
+      console.log("데이터:", (e as MessageEvent).data);
+    });
+
+    // event 정의 없이 모든 알림이 동일하게 전송될 경우 onmessage 사용
+    evtSource.onmessage = e => {
+      console.log("알람 수신: ", e.data);
+    };
+
     return () => {
       evtSource.close();
       console.log("SSE 연결 종료");
