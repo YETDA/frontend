0ac86917d8c4206e301bc9695c24a56c2fbedd4a
1juyoung <myjuyoung2@gmail.com>
2025-07-22 11:22:00 +0900

    feat: 사용자가 구매한 프로젝트 리스트 api 추가

--- apis/my/useUserOrderList.ts
+++ apis/my/useUserOrderList.ts
@@ -3,16 +3,21 @@ import axios from "axios";
 
 export function useUserOrderList() {
   const [userOrderList, setUserOrderData] = useState<any>(null);
+
   useEffect(() => {
     const fetchFollow = async () => {
       try {
         const res = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/project/count`,
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/project/purchase/me/list`,
           {
+            params: {
+              page: 0,
+              size: 20,
+              sort: "createdAt,desc",
+            },
             withCredentials: true,
           },
         );
-
         setUserOrderData(res.data.data);
       } catch (err) {
         console.error("로그인 필요 또는 인증 실패:", err);
