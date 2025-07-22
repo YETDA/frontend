56a6f80807b4bacf1e4e953f4bd3944dc0770df2
1juyoung <myjuyoung2@gmail.com>
2025-07-22 11:28:56 +0900

    style: 마이페이지 구매/후원 내역 카드 디자인 변경

--- app/(user)/my/components/ui/MyProjectCard.tsx
+++ app/(user)/my/components/ui/MyProjectCard.tsx
@@ -1,42 +1,53 @@
 import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ProjectContent } from "@/types/user/purchaseProject";
+import { Card, CardTitle } from "@/components/ui/card";
+import { getStatusBadge } from "../ProjectStatusBadge";
+
+interface ProjectProps {
+  id: number;
+  title: string;
+  sellCount: number;
+  projectStatus: string;
+  contentImageUrls: string;
+  hostName: string;
+}
 
 interface MyProjectCardProps {
-  project: ProjectContent;
+  project: ProjectProps;
 }
 
 export default function MyProjectCard({ project }: MyProjectCardProps) {
   return (
-    <Card className="w-full max-w-[280px] max-h-[330px] rounded-xl overflow-hidden flex flex-col">
-      <CardHeader>
-        <div className="relative w-full h-30">
+    <Card className="w-full max-w-[1000px] rounded-xl overflow-hidden flex flex-row items-center justify-between border border-gray-200 p-4 shadow-sm items-center">
+      {/* 왼쪽: 이미지 + 텍스트 */}
+      <div className="flex flex-row items-center gap-4">
+        {/* 이미지 */}
+        <div className="relative w-[64px] h-[64px] rounded-md overflow-hidden flex-shrink-0">
           <Image
             src={project.contentImageUrls}
             alt="Project Image"
             fill
-            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
+            sizes="64px"
             className="object-cover"
           />
         </div>
-      </CardHeader>
 
-      <CardContent className="flex flex-col justify-between gap-2.5 p-4">
-        <div className="text-xs color-[#868E96]">{project.hostName}</div>
-        <CardTitle className="text-md truncate w-full">
-          {project.title}
-        </CardTitle>
-        <div className="text-title">
-          {project.purchaseOptions?.length}회 판매
+        {/* 텍스트 정보 */}
+        <div className="flex flex-col justify-between">
+          <CardTitle className="text-sm font-semibold text-black">
+            {project.title}
+          </CardTitle>
+
+          <div className="flex items-center gap-1 text-xs text-gray-500 mt-1">
+            {getStatusBadge(project.projectStatus)}
+          </div>
         </div>
-        <Badge
-          variant="outline"
-          className="py-1 px-2 w-fit h-fit bg-gradient-to-r from-[#1F9EFF] to-[#0064FF] text-white border-none"
-        >
-          좋은 창작자
-        </Badge>
-      </CardContent>
+      </div>
+
+      {/* 금액 필드 추가 요청 해야함 */}
+      <div className="flex flex-col items-end gap-1">
+        <span className="text-blue-600 font-bold text-sm">500원</span>
+        <span className="text-gray-500 text-xs">구매 완료</span>
+      </div>
     </Card>
   );
 }
